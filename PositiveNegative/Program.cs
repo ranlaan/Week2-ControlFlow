85d74b8ca76ec63c0a850ee1bf3ed7d6b47ae196
Opilane TTHK <opilane@tthk.ee>
2021-09-09 11:14:08 +0300

    positivenegative

---
+++ PositiveNegative/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PositiveNegative
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm palub kasutajal sisestada numbrit -10 kuni 10
+            //programm kontrollib numbrit
+            //kui number on positiivne, konsool kuvab "positive"
+            //kui number on negatiivne, konsool kuvab "egative"
+            //kui number on0, siis konsool kuvab "null"
+
+            Console.WriteLine("Kirjuta üks number -10 kuni 10.");
+
+            int Number = Convert.ToInt32(Console.ReadLine());
+
+            if (Number < 0)
+            {
+                Console.WriteLine("Negative");
+            }
+            else if (Number > 0)
+            {
+                Console.WriteLine("Positive");
+            }
+            else
+            {
+                Console.WriteLine("Null");
+            }
+
+
+        }
+    }
+}
