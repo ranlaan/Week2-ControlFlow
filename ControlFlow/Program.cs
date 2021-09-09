fe7c14284bf599a075e2ab9c5b28baee2a06aeb7
Opilane TTHK <opilane@tthk.ee>
2021-09-09 10:32:30 +0300

    Add control flow

---
+++ ControlFlow/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ControlFlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja vanust
+            //kui kasutaja on vanem ku 18 siis konsoolis kuvatakse "oled piisavalt vana"
+            //kui kasutaja on noorem kui 18, siis konsoolis kuvatakse "oled liiga noor"
+            //kui kasutaja on 18, siis konsoolis kuvatakse "oled 18"
+
+            Console.WriteLine("Sisesta oma vanus.");
+            int Age = Convert.ToInt32(Console.ReadLine());
+
+            if (Age > 18)
+            {
+                Console.WriteLine("Oled piisavalt vana.");
+            }
+            else if (Age < 18)
+            {
+                Console.WriteLine("Oled liiga noor");
+            }
+            else
+            {
+                Console.WriteLine("Oled 18.");
+            }
+
+            Console.WriteLine("Kena päeva!");
+
+        }
+    }
+}
